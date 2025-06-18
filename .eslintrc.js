93eb9258c46c4eaf85dc2d7ae3efd73d9db5d5da
amit <amit@A-MSDT015.localdomain>
2025-06-18 17:40:31 +0530

    Switch ESLint config from .mjs to .eslintrc.js (CommonJS)

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+  },
+  rules: {
+    // You can add custom linting rules here
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
+
